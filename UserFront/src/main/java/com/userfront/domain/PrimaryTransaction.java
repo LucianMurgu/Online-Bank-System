a01ceeb55e10d796eb3e277a151882227bdc424e
Lucian Murgu <murgu_lucian@yahoo.com>
2017-10-13 15:35:05 +0200

    Stage 2.1

--- UserFront/src/main/java/com/userfront/domain/PrimaryTransaction.java
+++ UserFront/src/main/java/com/userfront/domain/PrimaryTransaction.java
@@ -3,7 +3,16 @@ package com.userfront.domain;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PrimaryTransaction {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private Date date;
 	private String description;
